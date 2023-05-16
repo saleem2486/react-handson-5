097ba47dc12b71aa43bc29363399bfe59b8a807d
unknown <maliksaleem32596@gmail.com>
2023-05-16 19:28:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import OnClick from './Components/OnClick';
+import OnHover from './Components/OnHover';
+import PureComponent from './Components/PureComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <OnClick/>
+     <OnHover/>
+     <PureComponent/>
     </div>
   );
 }
